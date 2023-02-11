ee5500295f937dcc16edfe360422d75622fa728e
Amo Mensah <amomensah300@gmail.com>
2023-02-11 18:39:00 +0000

    adding files

--- models/base_model.py
+++ models/base_model.py
@@ -15,7 +15,7 @@ class BaseModel():
 
     def __str__(self):
         """returns a a string containing class name, id and class atrributes """
-        return '[{}] <{}> {}'.format(self.__class__.__name__,
+        return '[{}] ({}) {}'.format(self.__class__.__name__,
                                      self.id, self.__dict__)
 
     def save(self):
