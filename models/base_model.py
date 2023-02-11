b7acd5c073f33df9a360518cd291314e3b3c40fb
Amo Mensah <amomensah300@gmail.com>
2023-02-11 18:13:54 +0000

    adding task 3

---
+++ models/base_model.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Module for base model """
+
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel():
+    """ Base class that other objects will inherit from. """
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return '[{}] <{}> {}'.format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """updates updated_to with current datetime """
+        self.created_at = datetime.now()
+
+    def to_dict(self):
+        """ returns a dictionary representation of an instance"""
+        new_dict = dict(self.__dict__)
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.__dict__['created_at'].isoformat()
+        new_dict['updated_at'] = self.__dict__['updated_at'].isoformat()
+        return(new_dict)
