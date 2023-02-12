d6b3431e8b7cd59e6e79013bc06e29eade6847b2
Amo Mensah <amomensah300@gmail.com>
2023-02-12 22:04:43 +0000

    adding correction

--- models/base_model.py
+++ models/base_model.py
@@ -9,6 +9,7 @@ from datetime import datetime
 class BaseModel():
     """ Base class that other objects will inherit from. """
     def __init__(self):
+        """ class constructor for base model """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
