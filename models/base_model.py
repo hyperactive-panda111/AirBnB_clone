05287f095faa4123f2965c56de98e75ee9988d65
Amo Mensah <amomensah300@gmail.com>
2023-02-13 04:42:02 +0000

    adding console related files

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 
 import uuid
 from datetime import datetime
-
+import models
 
 class BaseModel():
     """ base model class """
@@ -23,6 +23,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """returns a string """
@@ -32,6 +33,7 @@ class BaseModel():
     def save(self):
         """updates updated_to with current datetime """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """ returns a dictionary representation of an instance"""
